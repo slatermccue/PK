7462bc0aa38a5fe6142f8d6d911370590ba99857
slatermccue <slater.mccue@gmail.com>
2024-07-01 10:52:49 -0400

    work on finding and changing page name

--- pagifyXml.py
+++ pagifyXml.py
@@ -17,8 +17,25 @@ import lxml.etree as ET
 #note: the file may contain multiple slides, each of which needs to
 # have its page number modified in two locations
 input_file_path = sys.argv[1]
-with open(input_file_path)) as newSlide:
+with open(input_file_path) as newSlide:
     newSlideXml = newSlide.read()
+
+# define the namespace prefix mapping
+nsmap = {
+    'draw': 'urn:oasis:names:tc:opendocument:xmlns:drawing:1.0',
+    'pres': 'urn:oasis:names:tc:opendocument:xmlns:presentation:1.0',
+    'xlink': 'http://www.w3.org/1999/xlink'
+}
 
 slideParser = ET.XMLParser(remove_blank_text=True)
 slideRoot = ET.fromstring(newSlideXml, parser=slideParser)
+
+#print(slideRoot)
+#print(newSlideXml)
+update = slideRoot.attrib['{urn:oasis:names:tc:opendocument:xmlns:drawing:1.0}name']
+print(update)
+slideRoot.attrib['{urn:oasis:names:tc:opendocument:xmlns:drawing:1.0}name'] = 'page99'
+print()
+print("================") 
+pretty = ET.tostring(slideRoot, encoding="unicode", pretty_print=True)
+print(pretty)
